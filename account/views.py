1eda57a83cb6b568a8cbfbc5e2ba3465a0e46aef
김동건 <dgk2481@gmail.com>
2020-08-23 18:07:12 +0900

    [FIX] 코드 리팩토링

--- account/views.py
+++ account/views.py
@@ -1,4 +1,3 @@
-import re
 import bcrypt
 import requests
 import jwt
@@ -8,35 +7,35 @@ import re
 from django.views.decorators.csrf       import csrf_exempt
 from django.core.exceptions             import ObjectDoesNotExist
 from django.http                        import (
-    JsonResponse,
-    HttpResponse
+        JsonResponse,
+        HttpResponse
 )
-from django.http                        import 
 from django.views                       import View
 
 from .utils                             import decorator_login
 from dkinterest.settings                import (
-        SECRET_KEY, 
+        SECRET_KEY,
         ALGORITHM
-)
+)        
+
 from account.models                     import (
-        Account, 
+        Account,
         AccountInterest
 )
 
 class SignUpView(View):
     def post(self, request):
-        data = json.loads(request.body)
-        email_validation    = re.compile(
+        data                =   json.loads(request.body)
+        email_validation    =   re.compile(
             "^\S[a-zA-Z0-9+-_.]{4,28}\S@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
         )
         password_validation = re.compile(
-           "^(?=.{10,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$"
+            "^(?=.{10,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$"
         )
 
         try:
             if Account.objects.filter(email=data["email"]).exists():
-                return JsonResponse({"message": "Email Already Exists"}, status=400)
+                return JsonResponse({"message": "Email Already exists"}, status=400)
 
             if email_validation.match(data["email"]):
                 if password_validation.match(data["password"]):
@@ -47,15 +46,17 @@ class SignUpView(View):
                         email       =   data["email"],
                         password    =   password,
                         age         =   data["age"]
+                        # social_platform_id=1,
                     )
                     return HttpResponse(status=200)
 
-                return JsonResponse({"message":"INVALID_PASSWORD"}, status=400)
+                return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
 
-            return JsonResponse({"message":"INVALID_EMAIL"}, status=400)
+            return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
 
         except KeyError:
             return JsonResponse({"message": "INVALID_KEYS"}, status=400)
+
 
 class SignInView(View):
     def post(self, request):
@@ -67,7 +68,7 @@ class SignInView(View):
                 if bcrypt.checkpw(
                     data["password"].encode("utf-8"), account.password.encode("utf-8")
                 ):
-                    access_token   =   jwt.encode(
+                    access_token = jwt.encode(
                         {"email": account.email}, SECRET_KEY, algorithm=ALGORITHM
                     )
                     return JsonResponse({"access_token": access_token.decode("utf-8")}, status=200)
@@ -77,7 +78,8 @@ class SignInView(View):
             return HttpResponse(status=401)
 
         except KeyError:
-            return JsonResponse({"message":"INVALID_KEYS"}, status=400)
+            return JsonResponse({"message": "INVALID_KEYS"}, status=400)
+
 
 class KakaoLogInView(View):
     def post(self, request):
@@ -102,17 +104,18 @@ class KakaoLogInView(View):
             return JsonResponse({"access_token": access_token}, status=200)
 
         Account.objects.create(
-            email           =       kakao_user_email,
-            image_url       =       kakao_user_profile_img,
-            nickname        =       kakao_user_nickname,
+            email       =       kakao_user_email,
+            image_url   =       kakao_user_profile_img,
+            nickname    =       kakao_user_nickname,
         )
         return JsonResponse({"message": "SIGN UP SUCCESS"}, status=200)
+
 
 class InterestSaveView(View):
     @decorator_login
     def post(self, request):
         data        =       json.loads(request.body)
-        user_id     =       request.user.id
+        user_id     =   request.user.id
         try:
             selected_interest_list = data["interestId"]
         except KeyError:
@@ -122,4 +125,4 @@ class InterestSaveView(View):
             for i in selected_interest_list:
                 AccountInterest.objects.create(account_id=user_id, interest_id=i)
             return JsonResponse({"message": "SUCCESSFULLY SAVED"}, status=200)
-        return JsonResponse({"message": "CHOICE NEEDED"}, status=400)]
+        return JsonResponse({"message": "CHOICE NEEDED"}, status=400)
